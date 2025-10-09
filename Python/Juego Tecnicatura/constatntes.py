3f2ecf08354c1321e7fd85e4f52a2e50e59507cb
unknown <Jorgemathez14@gmail.com>
2025-10-09 18:59:12 -0300

    Juego python clase 1

---
+++ Python/Juego Tecnicatura/constatntes.py
@@ -0,0 +1,9 @@
+import as 
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+COLOR LASER = (0, 0, 255)
+
+ASSETS_PATH = es.path.join(es.path.dirname(__file__), "assets")
---
+++ Python/Juego Tecnicatura/main.py
@@ -0,0 +1,6 @@
+import pygame
+import sys
+import random
+import as
+from personaje import Personaje,Enemigo,Explocion
+from constantes import * 

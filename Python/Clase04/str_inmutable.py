938e36a81c97563bd0e345163f31b6a84276738a
unknown <Jorgemathez14@gmail.com>
2025-11-08 13:26:32 -0300

    update python

--- Python/Clase04/str_inmutable.py
+++ Python/Clase04/str_inmutable.py
@@ -1,8 +1,10 @@
-#help(str.capitalize)
+
+# help(str.capitalize)
 mensaje1 = 'hola mundo'
-mensaje2 = mensaje1.capitalize() #coloca la priemr letra en mayuscula
+mensaje2 = mensaje1.capitalize()
 print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
-print(f'Mensaje2: {mensaje2} id: {id(mensaje2)}')
+print(f'Mensaje2: {mensaje2}, id: {id(mensaje2)}')
+
 
 mensaje1 += ' Adios'
 print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
